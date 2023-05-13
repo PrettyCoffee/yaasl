a8c63c857a1c88723534d5d9b00ff027cd205679
PrettyCoffee <jonathan-holz@outlook.com>
2023-05-13 18:49:38 +0200

    chore: Fix builds

--- packages/vanilla/src/utils/logError.ts
+++ packages/vanilla/src/utils/logError.ts
@@ -1,6 +1,4 @@
-import { name } from "../../package.json"
-
-export const errorMessage = (text: string) => `${text} (${name})`
+export const errorMessage = (text: string) => `${text} (@yaasl/vanilla)`
 
 export const logError = (text: string, value?: unknown) =>
   console.error(errorMessage(text), ...(!value ? [] : ["\n\n", value]))
