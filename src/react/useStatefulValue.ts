f9a92c61c847a25295ec7292a23a87b55bd6943a
PrettyCoffee <jonathan-holz@outlook.com>
2023-08-06 18:00:15 +0200

    refactor(react): Use react useSyncExternalStore hook

--- src/react/useStatefulValue.ts
+++ src/react/useStatefulValue.ts
@@ -1,16 +1,9 @@
-import { useEffect, useRef, useState } from "react"
+import { useSyncExternalStore } from "react"
 
 import { Stateful } from "../core"
 
-export const useStatefulValue = <ValueType>(atom: Stateful<ValueType>) => {
-  const [state, setState] = useState(atom.get())
-  const unsubscribe = useRef<() => void>(() => null)
-
-  useEffect(() => {
-    unsubscribe.current()
-    unsubscribe.current = atom.subscribe(setState)
-    return () => unsubscribe.current()
-  }, [atom])
-
-  return state
-}
+export const useStatefulValue = <ValueType>(atom: Stateful<ValueType>) =>
+  useSyncExternalStore(
+    set => atom.subscribe(set),
+    () => atom.get()
+  )
