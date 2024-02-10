8f3fe45e60ad5c66014afb8c2de27d501441ca49
PrettyCoffee <jonathan-holz@outlook.com>
2024-02-10 03:47:49 +0100

    feat(preact): Add didInit state to useAtom hook

--- packages/preact/src/useAtom.ts
+++ packages/preact/src/useAtom.ts
@@ -1,6 +1,6 @@
 import { Atom, Stateful } from "@yaasl/core"
 import { SetStateAction } from "@yaasl/utils"
-import { useCallback } from "preact/hooks"
+import { useCallback, useEffect, useState } from "preact/hooks"
 
 import { useStatefulValue } from "./useStatefulValue"
 
@@ -22,17 +22,35 @@ export const useAtomValue = <ValueType>(atom: Stateful<ValueType>) =>
 export const useSetAtom = <ValueType>(atom: Atom<ValueType>) =>
   useCallback((next: SetStateAction<ValueType>) => atom.set(next), [atom])
 
+/** Use an atom's initialization state in the preact lifecycle.
+ *
+ *  @param atom Atom to be used.
+ *
+ *  @returns A boolean indicating if the atom has finished initializing yet.
+ **/
+export const useAtomDidInit = <ValueType>(atom: Atom<ValueType>) => {
+  const [didInit, setDidInit] = useState(atom.didInit === true)
+
+  useEffect(() => {
+    if (typeof atom.didInit === "boolean") return
+    void atom.didInit.then(() => setDidInit(true))
+  }, [atom.didInit])
+
+  return didInit
+}
+
 /** Use an atom's value and setter in the preact lifecycle.
  *
  * **Note:** Use `useAtomValue` or `useSetAtom` to use value or setter separately.
  *
  * @param atom Atom to be used.
  *
- * @returns A state value and state setter for the atom.
+ * @returns [value, setValue, didInit]
  **/
 export const useAtom = <ValueType>(atom: Atom<ValueType>) => {
   const state = useAtomValue(atom)
   const setState = useSetAtom(atom)
+  const didInit = useAtomDidInit(atom)
 
-  return [state, setState] as const
+  return [state, setState, didInit] as const
 }
