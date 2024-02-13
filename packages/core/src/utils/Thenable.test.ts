f749d219c3b39cceca9b9304ce4dfbae9ec98eca
PrettyCoffee <jonathan-holz@outlook.com>
2024-02-13 23:01:16 +0100

    test(core): Improve test structure for Thenable

--- packages/core/src/utils/Thenable.test.ts
+++ packages/core/src/utils/Thenable.test.ts
@@ -9,23 +9,6 @@ describe("Test Thenable", () => {
     expect(thenable).toHaveProperty("then")
   })
 
-  it(".then method returns a Thenable", () => {
-    const testFn = jest.fn()
-    const result = new Thenable("test").then(testFn)
-    expect(result).toBeInstanceOf(Thenable)
-    expect(testFn).toHaveBeenCalledWith("test")
-  })
-
-  it(".then method returns a Promise", async () => {
-    const testFn = jest.fn()
-    const result = new Thenable("test").then(value =>
-      sleep(5).then(() => testFn(value))
-    )
-    expect(result).toBeInstanceOf(Promise)
-    await result
-    expect(testFn).toHaveBeenCalledWith("test")
-  })
-
   it("Can be awaited", async () => {
     const thenable = new Thenable("test")
     expect(await thenable).toMatch("test")
@@ -36,55 +19,82 @@ describe("Test Thenable", () => {
     expect(await thenable).toBe(1)
   })
 
-  it("chains multiple sync .then calls", async () => {
-    const result = new Thenable(0)
-      .then(value => value + 1)
-      .then(value => value + 1)
+  describe(".then method", () => {
+    it("returns a Thenable", () => {
+      const testFn = jest.fn()
+      const result = new Thenable("test").then(testFn)
+      expect(result).toBeInstanceOf(Thenable)
+      expect(testFn).toHaveBeenCalledWith("test")
+    })
 
-    expect(result).not.toBeInstanceOf(Promise)
-    expect(await result).toBe(2)
+    it("returns a Promise", async () => {
+      const testFn = jest.fn()
+      const result = new Thenable("test").then(value =>
+        sleep(5).then(() => testFn(value))
+      )
+      expect(result).toBeInstanceOf(Promise)
+      await result
+      expect(testFn).toHaveBeenCalledWith("test")
+    })
+
+    it("is chainable with sync calls", async () => {
+      const result = new Thenable(0)
+        .then(value => value + 1)
+        .then(value => value + 1)
+
+      expect(result).not.toBeInstanceOf(Promise)
+      expect(await result).toBe(2)
+    })
+
+    it("is chainable with async calls", async () => {
+      const result = new Thenable(0)
+        .then(value => sleep(5).then(() => value + 1))
+        .then(value => sleep(5).then(() => value + 1))
+
+      expect(result).toBeInstanceOf(Promise)
+      expect(await result).toBe(2)
+    })
+
+    it("is chainable with sync and async mix", async () => {
+      const syncEnd = new Thenable(0)
+        .then(value => sleep(5).then(() => value + 1))
+        .then(value => value + 1)
+
+      expect(syncEnd).toBeInstanceOf(Promise)
+      expect(await syncEnd).toBe(2)
+
+      const asyncEnd = new Thenable(0)
+        .then(value => value + 1)
+        .then(value => sleep(5).then(() => value + 1))
+
+      expect(asyncEnd).toBeInstanceOf(Promise)
+      expect(await asyncEnd).toBe(2)
+    })
   })
 
-  it("chains multiple async .then calls", async () => {
-    const result = new Thenable(0)
-      .then(value => sleep(5).then(() => value + 1))
-      .then(value => sleep(5).then(() => value + 1))
+  describe("unwraps", () => {
+    it("unwraps nested Thenables", async () => {
+      const result = new Thenable(1).then(value => new Thenable(value + 1))
+      expect(result).toBeInstanceOf(Thenable)
+      expect(await result).toBe(2)
+    })
 
-    expect(result).toBeInstanceOf(Promise)
-    expect(await result).toBe(2)
-  })
+    it("unwraps nested Promises", async () => {
+      const result = new Thenable(1).then(value => Promise.resolve(value + 1))
+      expect(result).toBeInstanceOf(Promise)
+      expect(await result).toBe(2)
+    })
 
-  it("chains sync and async .then calls", async () => {
-    const syncEnd = new Thenable(0)
-      .then(value => sleep(5).then(() => value + 1))
-      .then(value => value + 1)
-
-    expect(syncEnd).toBeInstanceOf(Promise)
-    expect(await syncEnd).toBe(2)
-
-    const asyncEnd = new Thenable(0)
-      .then(value => value + 1)
-      .then(value => sleep(5).then(() => value + 1))
-
-    expect(asyncEnd).toBeInstanceOf(Promise)
-    expect(await asyncEnd).toBe(2)
-  })
-
-  it("Unwraps nested Thenables", () => {
-    const thenable = new Thenable("test")
-    const result = new Thenable().then(() => thenable.then(() => thenable))
-    expect(result).toBe(thenable)
-  })
-
-  it("Unwraps deep shit", async () => {
-    const result = new Thenable(1).then(value =>
-      Promise.resolve(value + 1).then(value =>
-        new Thenable(value + 1).then(value =>
-          Promise.resolve(value + 1).then(value => new Thenable(value + 1))
+    it("unwraps deep shit", async () => {
+      const result = new Thenable(1).then(value =>
+        Promise.resolve(value + 1).then(value =>
+          new Thenable(value + 1).then(value =>
+            Promise.resolve(value + 1).then(value => new Thenable(value + 1))
+          )
         )
       )
-    )
-    expect(result).toBeInstanceOf(Promise)
-    expect(await result).toBe(5)
+      expect(result).toBeInstanceOf(Promise)
+      expect(await result).toBe(5)
+    })
   })
 })
