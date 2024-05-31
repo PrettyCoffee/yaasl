4f9ea96a7f1ceaa0eacf2c54b4773de5f868d737
PrettyCoffee <jonathan-holz@outlook.com>
2024-05-31 04:45:38 +0200

    chore: Publish docs

---
+++ pages/examples/useThemeMode.tsx
@@ -0,0 +1,68 @@
+import {
+  createSelector,
+  useAtomValue,
+  localStorage,
+  createAtom,
+  createActions,
+} from "../../../packages/react/src"
+
+/// [systemMode]
+type ThemeMode = "dark" | "light"
+
+const getSystemMode = (): ThemeMode =>
+  window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"
+
+const systemMode = createAtom<ThemeMode>({
+  defaultValue: getSystemMode(),
+})
+
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", ({ matches }) =>
+    systemMode.set(matches ? "dark" : "light")
+  )
+/// [systemMode]
+
+/// [localMode]
+type ThemeModeOrSystem = ThemeMode | "system"
+const localMode = createAtom<ThemeModeOrSystem>({
+  defaultValue: "system",
+  effects: [localStorage()],
+})
+
+const nextMode = {
+  system: "dark",
+  dark: "light",
+  light: "system",
+} as const
+
+const modeActions = createActions(localMode, {
+  next: mode => nextMode[mode],
+})
+/// [localMode]
+
+/// [themeMode]
+const themeMode = createSelector(
+  [localMode, systemMode],
+  (mode, system): ThemeMode => (mode === "system" ? system : mode)
+)
+
+themeMode.subscribe(theme =>
+  document.documentElement.classList.toggle("dark", theme === "dark")
+)
+/// [themeMode]
+
+/// [useThemeMode]
+export const useThemeMode = () => {
+  const theme = useAtomValue(themeMode)
+  const mode = useAtomValue(localMode)
+  return { mode, theme, nextMode: modeActions.next }
+}
+/// [useThemeMode]
+
+/// [usage]
+export const Toggle = () => {
+  const { mode, nextMode } = useThemeMode()
+  return <button onClick={nextMode}>{mode}</button>
+}
+/// [usage]
