1a0ccaa75b15b1562853fd6ce3abd0aa3f0fff94
PrettyCoffee <66521452+PrettyCoffee@users.noreply.github.com>
2025-12-21 22:26:20 +0100

    chore: Replace jsdom by happy-dom

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config"
 export default defineConfig({
   test: {
     globals: true,
-    environment: "jsdom",
+    environment: "happy-dom",
     coverage: {
       include: ["packages/**/src/**/*.{ts,tsx}"],
       exclude: ["**/index.ts"],
